7d23c00afa081ca671fb9322ea6b8b476e669bfc
bini59 <myuni0234@gmail.com>
2023-03-02 18:09:17 +0900

    feat : log run code

--- index.js
+++ index.js
@@ -10,6 +10,31 @@ const run_code = (code) => {
     catch (e) { return e; }
 }
 
+const log_result = (input, result) => {
+
+    // set result
+    let result_div = document.createElement("div");
+    result_div.setAttribute("class", "console result");
+    result_div.innerText = result;
+
+    // set input
+    let input_div = document.createElement("div");
+    input_div.setAttribute("class", "console input");
+    input_div.innerHTML = `
+        <span class="console icon"></span>
+        <span class="console input-text">${input}</span>
+        <span class="console time"></span>
+    `
+
+    let log = document.createElement("div");
+    log.setAttribute("class", "console log");
+    log.appendChild(result_div);
+    log.appendChild(input_div);
+
+    let shell = document.getElementsByClassName("console-res")[0];
+    shell.appendChild(log)
+}
+
 let console_input = document.getElementById("console-input");
 console_input.addEventListener("keypress", (e) => {
     if (e.key != "Enter") return;
@@ -18,5 +43,7 @@ console_input.addEventListener("keypress", (e) => {
     if (text == "") return;
     if (text == "clear") return;
     let result = run_code(text);
+    log_result(text, result);
+    e.target.value = ""
 
 })
