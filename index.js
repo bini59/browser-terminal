55d82e6e743abdd0aacced304d913c0ac6cef342
bini59 <myuni0234@gmail.com>
2023-03-02 17:57:56 +0900

    feat : run code function
    - input : String
    run javascript string code

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ console.log = (...args) => {
     console.old(...args);
     return args;
 }
+
+
+const run_code = (code) => {
+    try { return new Function(code)(); }
+    catch (e) { return e; }
+}
+
