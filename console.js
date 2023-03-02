cdb3f7f61493bed93ac904a23dec4bf2f375985c
bini59 <myuni0234@gmail.com>
2023-03-02 21:43:27 +0900

    feat : complete word

--- console.js
+++ console.js
@@ -10,24 +10,23 @@ const run_code = (code) => {
     catch (e) { return e; }
 }
 
-const log_result = (input, result) => {
+const log_result = (input, result, err=false) => {
 
     // set result
     let result_div = document.createElement("div");
-    result_div.setAttribute("class", "console result");
-    result_div.innerText = result;
+    result_div.setAttribute("class", "console-result-div");
+    result_div.innerHTML = `<span>${result}</br></br>`;
 
     // set input
     let input_div = document.createElement("div");
-    input_div.setAttribute("class", "console input");
+    input_div.setAttribute("class", "console-input-div");
     input_div.innerHTML = `
-        <span class="console icon"></span>
-        <span class="console input-text">${input}</span>
-        <span class="console time"></span>
+        <span class="term-arrow" readonly="true">➜</span> <span class="term-tilde">~</span> 
+        <div id="console-input" style="color:${(err ? "#ff625e" : "#05ba25")};font-weight:bold;">${input}</br></div>
     `
 
     let log = document.createElement("div");
-    log.setAttribute("class", "console log");
+    log.setAttribute("class", "console-log");
     log.appendChild(input_div);
     log.appendChild(result_div);
 
@@ -39,15 +38,24 @@ const clear_shell = () => {
     document.getElementsByClassName("console-res")[0].innerHTML = ""
 }
 
+let console_input_wrapper = document.getElementsByClassName("console")[0];
+console_input_wrapper.addEventListener("click", () => {
+    document.getElementById("console-input").focus();
+})
+
 let console_input = document.getElementById("console-input");
 console_input.addEventListener("keypress", (e) => {
+    if (e.key == "backspace") console.old("asdf");
     if (e.key != "Enter") return;
 
-    let text = e.target.value;
+    e.preventDefault()
+
+    let text = e.target.innerText;
+    console.old(text)
     if (text == "") return;
     if (text == "clear") { clear_shell(); e.target.value = ""; return; }
     let result = run_code(text);
-    log_result(text, result);
-    e.target.value = ""
+    log_result(text, result, (result instanceof Error ? true : false));
+    e.target.innerText = ""
 
 })
